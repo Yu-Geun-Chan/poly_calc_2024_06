85cbd8d2d0e2398f274c14ccbbc808a957f17cd2
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-25 15:41:08 +0900

    괄í불필요한 괄호 제거 강사님 풀이

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -5,19 +5,19 @@ import java.util.stream.Collectors;
 
 public class Calc {
     public static int run(String exp) {
+        // 괄호 제거
+        exp = stripOuterBrakets(exp);
 
         if (!exp.contains(" ")) {
             return Integer.parseInt(exp);
         }
 
-        exp = exp.replaceAll("\\(", "");
-        exp = exp.replaceAll("\\)", "");
-
+        boolean needToDivide = exp.contains(" / ");
         boolean needToMulti = exp.contains(" * ");
         boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
         boolean needToCompound = needToMulti && needToPlus;
 
-        if(needToCompound) {
+        if (needToCompound) {
             exp = exp.replaceAll("- ", "+ -");
 
             String[] bits = exp.split(" \\+ ");
@@ -26,10 +26,9 @@ public class Calc {
                     .mapToInt(Calc::run)
                     .mapToObj(e -> e + "")
                     .collect(Collectors.joining(" + "));
-
-
             return run(newExp);
-            } else if(needToPlus) {
+
+        } else if (needToPlus) {
             exp = exp.replaceAll("- ", "+ -");
 
             String[] bits = exp.split(" \\+ ");
@@ -48,9 +47,19 @@ public class Calc {
                 sum *= Integer.parseInt(bits[i]);
             }
             return sum;
+
         }
         throw new RuntimeException("올바른 계산식이 아니야");
     }
+
+    private static String stripOuterBrakets(String exp) {
+        // index 첫 시작이 여는괄호, index 마지막이 닫는 괄호일 때 변수 exp에다가 괄호 안의 것들을 넣겠다.
+        if (exp.charAt(0) == ('(') && exp.charAt(exp.length() - 1) == (')')) {
+            exp = exp.substring(1, exp.length() - 1);
+        }
+        return exp;
+    }
 }
+
 
 
